96c590dd4a7584a58822d6da4f0b4e36f1566391
Joseph Dayo <joseph.dayo@gmail.com>
2013-04-22 12:22:17 +0800

    Working oisems message encode/decode

--- src/org/oisems/client/Main.java
+++ src/org/oisems/client/Main.java
@@ -4,7 +4,6 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.oisems.client.message.OisemsMessage;
 
 public class Main {
-
 	public static void main(String args[]) {
 		OisemsClientDevice sender_client_device = Utils.createClientDevice();
 		OisemsClientDevice recipient_client_device = Utils.createClientDevice();
@@ -16,15 +15,14 @@ public class Main {
 		message.setSender(sender_client_device.getPublicKey());
 		message.setRecipient(recipient_client_device.getPublicKey());
 		message.setTimestamp(System.currentTimeMillis());
-		message.setMessage("Hello World!!!!!");
+		message.setMessage("This is something else Hello World!!!!!. Yes it is");
 		message.setMessageId(DigestUtils.sha256Hex(Double.toString(Math.random())));
 		
 		byte mes_ser[] = message.toBytes(sender_client_device.getPrivateKey());
 		
 		OisemsMessage message2 = new OisemsMessage();
 		message2.fromBytes(mes_ser, recipient_client_device.getPrivateKey());
-		
-		//System.out.println(message.toHexString(sender_client_device.getPrivateKey()));
+		System.out.println("decoded message = " + message2.getMessage());
 	}
 	
 }
--- src/org/oisems/client/message/OisemsMessage.java
+++ src/org/oisems/client/message/OisemsMessage.java
@@ -75,21 +75,23 @@ public class OisemsMessage {
 		try {
 			PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(
 					new X509EncodedKeySpec(Base64.decodeBase64(recipient)));
-			Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+			Cipher cipher = Cipher
+					.getInstance("RSA");
 			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-
 			byte[] header = stringToBytesASCII("OISEMS");
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			byte[] salt = md.digest(Double.toString(Math.random()).getBytes());
-
-			ByteBuffer buffer = ByteBuffer.allocate(64 *20);
-			ByteBuffer finalbuffer = ByteBuffer.allocate(64 * 20);
+			ByteBuffer buffer = ByteBuffer.allocate(1792);
 			buffer.put(header);
 			buffer.put(salt);
-			buffer.put(message.getBytes(Charset.forName("UTF-8")));
-			finalbuffer.put(cipher.doFinal(buffer.array()));
+			System.out.println("salt size = " + salt.length);
+			byte message_bytes[] = message.getBytes(Charset.forName("UTF-8")); 
+			buffer.putInt(message_bytes.length);
+			buffer.put(message_bytes);
 			
-			return finalbuffer.array();
+			byte[] result = blockCipher(cipher,buffer.array(),Cipher.ENCRYPT_MODE);
+			System.out.println("encrypted size = " + result.length);
+			return result;
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -112,27 +114,92 @@ public class OisemsMessage {
 		return null;
 	}
 
+	private byte[] blockCipher(Cipher cipher, byte[] bytes, int mode)
+			throws IllegalBlockSizeException, BadPaddingException {
+		// string initialize 2 buffers.
+		// scrambled will hold intermediate results
+		byte[] scrambled = new byte[0];
+
+		// toReturn will hold the total result
+		byte[] toReturn = new byte[0];
+		// if we encrypt we use 100 byte long blocks. Decryption requires 128
+		// byte long blocks (because of RSA)
+		int length = (mode == Cipher.ENCRYPT_MODE) ? 100 : 128;
+
+		// another buffer. this one will hold the bytes that have to be modified
+		// in this step
+		byte[] buffer = new byte[length];
+
+		for (int i = 0; i < bytes.length; i++) {
+
+			// if we filled our buffer array we have our block ready for de- or
+			// encryption
+			if ((i > 0) && (i % length == 0)) {
+				// execute the operation
+				scrambled = cipher.doFinal(buffer);
+				// add the result to our total result.
+				toReturn = append(toReturn, scrambled);
+				// here we calculate the length of the next buffer required
+				int newlength = length;
+
+				// if newlength would be longer than remaining bytes in the
+				// bytes array we shorten it.
+				if (i + length > bytes.length) {
+					newlength = bytes.length - i;
+				}
+				// clean the buffer array
+				buffer = new byte[newlength];
+			}
+			// copy byte into our buffer.
+			buffer[i % length] = bytes[i];
+		}
+
+		// this step is needed if we had a trailing buffer. should only happen
+		// when encrypting.
+		// example: we encrypt 110 bytes. 100 bytes per run means we "forgot"
+		// the last 10 bytes. they are in the buffer array
+		scrambled = cipher.doFinal(buffer);
+
+		// final step before we can return the modified data.
+		toReturn = append(toReturn, scrambled);
+
+		return toReturn;
+	}
+
+	private byte[] append(byte[] prefix, byte[] suffix) {
+		byte[] toReturn = new byte[prefix.length + suffix.length];
+		for (int i = 0; i < prefix.length; i++) {
+			toReturn[i] = prefix[i];
+		}
+		for (int i = 0; i < suffix.length; i++) {
+			toReturn[i + prefix.length] = suffix[i];
+		}
+		return toReturn;
+	}
+
 	public void fromBytes(byte message[], String privateKey) {
 		ByteBuffer buffer = ByteBuffer.wrap(message);
 		int version = buffer.get();
 		System.out.println("OISEMS version " + version);
-		byte senderPublicKey[] = new byte[94];
-		byte recipientPublicKey[] = new byte[94];
+		byte senderPublicKey[] = new byte[162];
+		byte recipientPublicKey[] = new byte[162];
 		byte messageId[] = new byte[64];
-		byte ds[] = new byte[64];
-		byte raw_message[] = new byte[64*20];
+		byte ds[] = new byte[128];
+		
 		buffer.get(senderPublicKey);
 		buffer.get(recipientPublicKey);
 		buffer.get(messageId);
 		long timestamp = buffer.getLong();
 		part = buffer.getInt();
 		buffer.get(ds);
+		int raw_message_length = buffer.getInt();
+		byte raw_message[] = new byte[raw_message_length];
 		buffer.get(raw_message);
-		//verify DS
+		// verify DS
 		try {
 			Signature sig = Signature.getInstance("MD5WithRSA");
-			PublicKey senderIdentity  = KeyFactory.getInstance("RSA").generatePublic(
-					new X509EncodedKeySpec(senderPublicKey)); 
+			PublicKey senderIdentity = KeyFactory.getInstance("RSA")
+					.generatePublic(new X509EncodedKeySpec(senderPublicKey));
 			sig.initVerify(senderIdentity);
 			sig.update(raw_message);
 			if (sig.verify(ds)) {
@@ -140,7 +207,6 @@ public class OisemsMessage {
 			} else {
 				System.out.println("Sender identity mismatch");
 			}
-			
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -154,9 +220,55 @@ public class OisemsMessage {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		PKCS8EncodedKeySpec kspec = new PKCS8EncodedKeySpec(
+				Base64.decodeBase64(privateKey));
+		KeyFactory kf;
+		try {
+			kf = KeyFactory.getInstance("RSA");
+			PrivateKey privKey = kf.generatePrivate(kspec);
+			Cipher cipher = Cipher
+					.getInstance("RSA");
+			cipher.init(Cipher.DECRYPT_MODE, privKey);
+			byte[] decrypted = blockCipher(cipher, raw_message,Cipher.DECRYPT_MODE);
+			ByteBuffer messageBuffer = ByteBuffer.wrap(decrypted);
+			
+			byte[] header = new byte[6];
+			byte[] salt = new byte[16];
+			
+			messageBuffer.get(header);
+			messageBuffer.get(salt);
+
+			int message_length = messageBuffer.getInt();
+			byte[] decrypted_message = new byte[message_length];
+			messageBuffer.get(decrypted_message,0, message_length);
+			String msg = new String(decrypted_message, Charset.forName("UTF-8"));
+			
+			
+			this.setMessage(msg);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		//Decrypt the message
+
 	}
-	
+
 	public byte[] toBytes(String privateKey) {
 		ByteBuffer buffer = ByteBuffer.allocate(MAX_SIZE);
 		System.out.println("start " + buffer.position());
@@ -185,7 +297,9 @@ public class OisemsMessage {
 			System.out.println("sig position = " + buffer.position());
 			buffer.put(sig.sign());
 			System.out.println("sig position = " + buffer.position());
+			buffer.putInt(payload.length);
 			buffer.put(payload);
+			System.out.println("end position = " + buffer.position());
 			return buffer.array();
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
